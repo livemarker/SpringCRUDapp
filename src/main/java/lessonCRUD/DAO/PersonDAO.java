0335d7f310503cf3769bfb448c9e58720a06d91c
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-04-08 15:40:45 +0300

    added POST

--- src/main/java/lessonCRUD/DAO/PersonDAO.java
+++ src/main/java/lessonCRUD/DAO/PersonDAO.java
@@ -12,19 +12,25 @@ public class PersonDAO {
     private static int PEOPLE_COUNT;
 
     {
-        people =new ArrayList<>();
+        people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"John"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
 
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
